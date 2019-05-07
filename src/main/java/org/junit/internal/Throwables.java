cf9e18b7ea8d16b09b2348a4eea7547d4bd3e207
Tommi Tallgren <tommi.tallgren@softagram.com>
2019-05-07 08:26:21 +0300

    adding arraylist back to imports

--- src/main/java/org/junit/internal/Throwables.java
+++ src/main/java/org/junit/internal/Throwables.java
@@ -7,7 +7,7 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.lang.reflect.Method;
 import java.util.AbstractList;
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
