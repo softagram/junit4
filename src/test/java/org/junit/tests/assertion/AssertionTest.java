ed892d222b466059a3aeb98b97b8b17a30aeb233
Tommi Tallgren <tommi.tallgren@softagram.com>
2019-05-07 08:28:22 +0300

    added assertThat back to import

--- src/test/java/org/junit/tests/assertion/AssertionTest.java
+++ src/test/java/org/junit/tests/assertion/AssertionTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
-//import static org.junit.Assert.assertThat; (Removed for PR test)
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.fail;
